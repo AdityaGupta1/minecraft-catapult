10ea5c6bbcb8cb017486bd087374942e5f35e40d
Aditya Gupta <adityag@gmail.com>
2017-06-14 14:43:43 -0700

    removed unnecessary cow behaviors from catapult

--- src/main/java/org/sdoaj/catapult/EntityCatapult.java
+++ src/main/java/org/sdoaj/catapult/EntityCatapult.java
@@ -3,145 +3,73 @@ package org.sdoaj.catapult;
 import net.minecraft.block.Block;
 import net.minecraft.entity.EntityAgeable;
 import net.minecraft.entity.SharedMonsterAttributes;
-import net.minecraft.entity.ai.EntityAIFollowParent;
-import net.minecraft.entity.ai.EntityAILookIdle;
-import net.minecraft.entity.ai.EntityAIMate;
-import net.minecraft.entity.ai.EntityAIPanic;
-import net.minecraft.entity.ai.EntityAISwimming;
-import net.minecraft.entity.ai.EntityAITempt;
-import net.minecraft.entity.ai.EntityAIWander;
-import net.minecraft.entity.ai.EntityAIWatchClosest;
 import net.minecraft.entity.passive.EntityCow;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.ChatComponentText;
+import net.minecraft.util.EnumChatFormatting;
 import net.minecraft.world.World;
 
-public class EntityCatapult extends EntityCow
-{
-    private static final String __OBFID = "CL_00001640";
+public class EntityCatapult extends EntityCow {
+	public EntityCatapult(World world) {
+		super(world);
+		this.setSize(0.9F, 1.3F);
+	}
 
-    public EntityCatapult(World p_i1683_1_)
+	public boolean isAIEnabled() {
+		return false;
+	}
+
+	protected void applyEntityAttributes() {
+		super.applyEntityAttributes();
+		this.getEntityAttribute(SharedMonsterAttributes.maxHealth).setBaseValue(1D);
+		this.getEntityAttribute(SharedMonsterAttributes.movementSpeed).setBaseValue(0D);
+	}
+
+	protected String getLivingSound() {
+		return null;
+	}
+
+	protected String getHurtSound() {
+		return null;
+	}
+
+	protected String getDeathSound() {
+		return null;
+	}
+
+	protected void func_145780_a(int p_145780_1_, int p_145780_2_, int p_145780_3_, Block p_145780_4_) {
+		return;
+	}
+
+	protected float getSoundVolume() {
+		return 0F;
+	}
+
+	protected Item getDropItem() {
+		return Item.getItemFromBlock(Blocks.log);
+	}
+
+	protected void dropFewItems(boolean p_70628_1_, int p_70628_2_) {
+
+		int amountToDrop = this.getRNG().nextInt(3);
+
+		for (int i = 0; i < amountToDrop; i++) {
+			if (this.isBurning()) {
+				// charcoal
+				this.dropItem(new ItemStack(Items.coal, 1, 1).getItem(), 1);
+			} else {
+				this.dropItem(Item.getItemFromBlock(Blocks.log), 1);
+			}
+		}
+	}
+
+	public boolean interact(EntityPlayer player)
     {
-        super(p_i1683_1_);
-        this.setSize(0.9F, 1.3F);
-    }
-
-    /**
-     * Returns true if the newer Entity AI code should be run
-     */
-    public boolean isAIEnabled()
-    {
-        return false;
-    }
-
-    protected void applyEntityAttributes()
-    {
-        super.applyEntityAttributes();
-        this.getEntityAttribute(SharedMonsterAttributes.maxHealth).setBaseValue(10.0D);
-        this.getEntityAttribute(SharedMonsterAttributes.movementSpeed).setBaseValue(0D);
-    }
-
-    /**
-     * Returns the sound this mob makes while it's alive.
-     */
-    protected String getLivingSound()
-    {
-        return "mob.cow.say";
-    }
-
-    /**
-     * Returns the sound this mob makes when it is hurt.
-     */
-    protected String getHurtSound()
-    {
-        return "mob.cow.hurt";
-    }
-
-    /**
-     * Returns the sound this mob makes on death.
-     */
-    protected String getDeathSound()
-    {
-        return "mob.cow.hurt";
-    }
-
-    protected void func_145780_a(int p_145780_1_, int p_145780_2_, int p_145780_3_, Block p_145780_4_)
-    {
-        this.playSound("mob.cow.step", 0.15F, 1.0F);
-    }
-
-    /**
-     * Returns the volume for the sounds this mob makes.
-     */
-    protected float getSoundVolume()
-    {
-        return 0.4F;
-    }
-
-    protected Item getDropItem()
-    {
-        return Items.leather;
-    }
-
-    /**
-     * Drop 0-2 items of this living's type. @param par1 - Whether this entity has recently been hit by a player. @param
-     * par2 - Level of Looting used to kill this mob.
-     */
-    protected void dropFewItems(boolean p_70628_1_, int p_70628_2_)
-    {
-        int j = this.rand.nextInt(3) + this.rand.nextInt(1 + p_70628_2_);
-        int k;
-
-        for (k = 0; k < j; ++k)
-        {
-            this.dropItem(Items.leather, 1);
-        }
-
-        j = this.rand.nextInt(3) + 1 + this.rand.nextInt(1 + p_70628_2_);
-
-        for (k = 0; k < j; ++k)
-        {
-            if (this.isBurning())
-            {
-                this.dropItem(Items.cooked_beef, 1);
-            }
-            else
-            {
-                this.dropItem(Items.beef, 1);
-            }
-        }
-    }
-
-    /**
-     * Called when a player interacts with a mob. e.g. gets milk from a cow, gets into the saddle on a pig.
-     */
-    public boolean interact(EntityPlayer p_70085_1_)
-    {
-        ItemStack itemstack = p_70085_1_.inventory.getCurrentItem();
-
-        if (itemstack != null && itemstack.getItem() == Items.bucket && !p_70085_1_.capabilities.isCreativeMode)
-        {
-            if (itemstack.stackSize-- == 1)
-            {
-                p_70085_1_.inventory.setInventorySlotContents(p_70085_1_.inventory.currentItem, new ItemStack(Items.milk_bucket));
-            }
-            else if (!p_70085_1_.inventory.addItemStackToInventory(new ItemStack(Items.milk_bucket)))
-            {
-                p_70085_1_.dropPlayerItemWithRandomChoice(new ItemStack(Items.milk_bucket, 1, 0), false);
-            }
-
-            return true;
-        }
-        else
-        {
-            return super.interact(p_70085_1_);
-        }
-    }
-
-    public EntityCatapult createChild(EntityAgeable p_90011_1_)
-    {
-        return new EntityCatapult(this.worldObj);
+		player.addChatComponentMessage(new ChatComponentText(EnumChatFormatting.AQUA + "" + EnumChatFormatting.BOLD + "catapult"));
+		return true;
     }
 }
