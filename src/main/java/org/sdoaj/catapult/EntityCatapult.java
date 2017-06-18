8f9b5a9e362f5cf51c7b5a5b276ed0ebf3806979
AdityaGupta1 <adityag@gmail.com>
2017-06-18 10:53:42 -0700

    cows now ride on falling blocks, fixed #3 (EntityAnimal rather than EntityCow), other minor changes

--- src/main/java/org/sdoaj/catapult/EntityCatapult.java
+++ src/main/java/org/sdoaj/catapult/EntityCatapult.java
@@ -1,18 +1,28 @@
 package org.sdoaj.catapult;
 
+import java.util.Arrays;
+import java.util.List;
+
 import javax.vecmath.Vector3d;
 
 import net.minecraft.block.Block;
+import net.minecraft.entity.EntityAgeable;
 import net.minecraft.entity.SharedMonsterAttributes;
+import net.minecraft.entity.item.EntityFallingBlock;
+import net.minecraft.entity.passive.EntityAnimal;
 import net.minecraft.entity.passive.EntityCow;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.ChatComponentText;
 import net.minecraft.util.EnumChatFormatting;
 import net.minecraft.world.World;
 
-public class EntityCatapult extends EntityCow {
+public class EntityCatapult extends EntityAnimal {
+	List<Block> blocks = Arrays
+			.asList(new Block[] { Blocks.diamond_block, Blocks.gold_block, Blocks.iron_block, Blocks.emerald_block });
+
 	public EntityCatapult(World world) {
 		super(world);
 		this.setSize(2F, 1.5F);
@@ -20,12 +30,6 @@ public class EntityCatapult extends EntityCow {
 
 	public boolean isAIEnabled() {
 		return false;
-	}
-
-	protected void applyEntityAttributes() {
-		super.applyEntityAttributes();
-		this.getEntityAttribute(SharedMonsterAttributes.maxHealth).setBaseValue(1D);
-		this.getEntityAttribute(SharedMonsterAttributes.movementSpeed).setBaseValue(0D);
 	}
 
 	protected String getLivingSound() {
@@ -47,9 +51,29 @@ public class EntityCatapult extends EntityCow {
 	protected float getSoundVolume() {
 		return 0F;
 	}
+	
+	private Vector3d multiplyVectors(Vector3d vector1, Vector3d vector2) {
+		return new Vector3d(vector1.x * vector2.x, vector1.y * vector2.y, vector1.z * vector2.z);
+	}
 
-	protected Item getDropItem() {
-		return Item.getItemFromBlock(Blocks.log);
+	@Override
+	public ItemStack getHeldItem() {
+		return null;
+	}
+
+	@Override
+	public ItemStack getEquipmentInSlot(int p_71124_1_) {
+		return null;
+	}
+
+	@Override
+	public void setCurrentItemOrArmor(int p_70062_1_, ItemStack p_70062_2_) {
+		// do nothing
+	}
+
+	@Override
+	public ItemStack[] getLastActiveItems() {
+		return null;
 	}
 
 	protected void dropFewItems(boolean p_70628_1_, int p_70628_2_) {
@@ -60,41 +84,47 @@ public class EntityCatapult extends EntityCow {
 			this.dropItem(Item.getItemFromBlock(Blocks.log), 1);
 		}
 	}
-	
+
 	public boolean interact(EntityPlayer player) {
 		World world = player.getEntityWorld();
-		
+
 		double angle = Main.angle;
 		double power = Main.power;
-		
+
 		if (!Main.parametersSet) {
-			player.addChatComponentMessage(
-					new ChatComponentText(EnumChatFormatting.DARK_RED + "" + EnumChatFormatting.BOLD + "Use \"/catapult <angle> <power>\" first!"));
+			player.addChatComponentMessage(new ChatComponentText(EnumChatFormatting.DARK_RED + ""
+					+ EnumChatFormatting.BOLD + "Use \"/catapult <angle> <power>\" first!"));
 			return false;
 		}
-		
+
 		if (!world.isRemote) {
 			return false;
 		}
-		
+
 		player.addChatComponentMessage(
 				new ChatComponentText(EnumChatFormatting.AQUA + "" + EnumChatFormatting.BOLD + "Launching cow..."));
-		
+
 		EntityCow cow = new EntityCow(world);
 		cow.setLocationAndAngles(this.posX, this.posY, this.posZ, 0, 0);
-		
-		Vector3d initialVector = new Vector3d(Math.cos(Math.toRadians(angle)) * power, Math.sin(Math.toRadians(angle)) * power, Math.cos(Math.toRadians(angle)) * power);
-//		Vector3d multiplyVector = new Vector3d();
+
+		EntityFallingBlock block = new EntityFallingBlock(world, this.posX, this.posY, this.posZ,
+				blocks.get((int) Math.floor(Math.random() * blocks.size())));
+		Vector3d initialVector = new Vector3d(Math.cos(Math.toRadians(angle)) * power,
+				Math.sin(Math.toRadians(angle)) * power, Math.cos(Math.toRadians(angle)) * power);
 		Vector3d multiplyVector = new Vector3d(0, 1, -1);
 		Vector3d velocity = multiplyVectors(initialVector, multiplyVector);
-		cow.setVelocity(velocity.x, velocity.y, velocity.z);
-		
+		block.setVelocity(velocity.x, velocity.y, velocity.z);
+
+		cow.mountEntity(block);
+
+		world.spawnEntityInWorld(block);
 		world.spawnEntityInWorld(cow);
-		
+
 		return true;
 	}
-	
-	private Vector3d multiplyVectors(Vector3d vector1, Vector3d vector2) {
-		return new Vector3d(vector1.x * vector2.x, vector1.y * vector2.y, vector1.z * vector2.z);
+
+	@Override
+	public EntityAgeable createChild(EntityAgeable p_90011_1_) {
+		return null;
 	}
 }
