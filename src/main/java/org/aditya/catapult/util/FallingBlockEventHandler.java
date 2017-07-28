2b3b47cca07f3067aba2589b2b20b22bb532fdf8
Dan Hardiker <dan@hardiker.co.uk>
2017-07-28 17:49:10 +0100

    easier explosions

--- src/main/java/org/aditya/catapult/util/FallingBlockEventHandler.java
+++ src/main/java/org/aditya/catapult/util/FallingBlockEventHandler.java
@@ -5,6 +5,7 @@ import java.util.TimerTask;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.item.EntityFallingBlock;
+import net.minecraft.world.World;
 import net.minecraftforge.event.entity.living.LivingEvent.LivingUpdateEvent;
 
 public class FallingBlockEventHandler {
@@ -28,5 +29,12 @@ public class FallingBlockEventHandler {
 		}
 
 		ridingEntity.setVelocity(0, 0, 0);
+		
+		World world = ridingEntity.worldObj;
+		double x = ridingEntity.posX;
+		double y = ridingEntity.posY;
+		double z = ridingEntity.posZ;
+		
+		
 	}
 }
